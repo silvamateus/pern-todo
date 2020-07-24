fc1f9622e63a9a1c8dea3a1e036635c2772a9f13
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2020-07-24 18:04:43 -0300

    fix typos and uncoment connection

--- client/src/components/inputTodos.js
+++ client/src/components/inputTodos.js
@@ -1,29 +1,28 @@
 import React, {Fragment, useState} from 'react';
 
 const InputTodo = () => {
-    const {descritpion, setDescription} = useState("");
+    const [description, setDescription] = useState("");
     const onSubmitForm = async e => {
         e.preventDefault();
-        console.log(e)
-        // try {
-        //     const body = {descritpion};
-        //     const response = await fetch("http://localhost:5000/todos",
-        //     {
-        //         method: "POST",
-        //         headers: {"Content-Type": "application/json"},
-        //         body: JSON.stringify(body)
-        //     } );
-        //     console.log(response)
-        // } catch (err) {
-        //     console.error(err.message)
-        // }
+        try {
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            } );
+            console.log(response)
+        } catch (err) {
+            console.error(err.message)
+        }
     }
     return ( <Fragment>
         <h1 className="text-center mt-5" >Pern Todo List</h1>
         <form className="d-flex mt-5" onSubmit={ onSubmitForm}>
             <input type="text" 
             className="form-control" 
-            value={descritpion}
+            value={description}
             onChange={ e => setDescription(e.target.value)}
             />
             <button className="btn btn-success">Add</button>
