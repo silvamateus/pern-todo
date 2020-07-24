3162d1623bb33058443db415d8e5e88762990908
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2020-07-24 19:33:24 -0300

    add functionalities

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React, {Fragment} from 'react';
 import './App.css';
 
 import InputTodo from './components/inputTodos'
-
+import ListTodos from './components/listTodos'
 function App() {
   return (
     <Fragment>
       <div className="container">
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
